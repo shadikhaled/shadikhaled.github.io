4a47518896ca33b3ecee1d467a954892415da656
Shadi Khaled <shadikhaled777@gmail.com>
2017-04-05 15:18:02 +0300

    created the basic structure

---
+++ js/projects.js
@@ -0,0 +1,31 @@
+var current_url='https://api.github.com/users/shadiKhaled/repos?sort=updated&per_page=5'
+$(document).ready(function(){
+	bindCurrentClickEvent();
+})
+
+function bindCurrentClickEvent (){
+	$('#btnCurrent').click(getCurrentRepositories)
+}
+function getCurrentRepositories(e){
+	$('#result').html('')
+	$.get(current_url,function(repos){
+		showCurrentRepositories(repos)
+	})
+
+}
+function showCurrentRepositories(repos){
+	repos.forEach(function(repo){
+		var date= new Date(repo.updated_at)
+		$('#result').append(`<div><a href='${repo.html_url}' target='_blank'>${repo.name}</a> Languge: ${repo.language}<p>Updated at: ${formatDate(date)}</p></div>`)
+	})
+}
+function formatDate(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var ampm = hours >= 12 ? 'pm' : 'am';
+	hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+}
