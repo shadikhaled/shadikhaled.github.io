944423092552227bfb06a4b3c8d30b2c209ffb05
Shadi Khaled <shadikhaled777@gmail.com>
2017-04-08 22:46:36 +0300

    add function to skills fail

--- js/skills.js
+++ js/skills.js
@@ -1,39 +1,18 @@
+$(document).ready(function(){
+	function getSkills(){
+skills_url = “skills.json”
+$.get(skills_url,function(data){
+	showSkills(data)
 
-3function favSubmitted() {
-	$("#fav").click(function(evant){
-		event.preventDefault();
-		var favName= $("#favName").val();
-		var favLang= $("#favLang").val();
-		var favUrl= $("#favUrl").val();
-		getAjax(favName,favLang,favUrl);
-		event.stopPropagation();
+
+})
+	
+function getCurrentRepositories(e){
+	$('#result').html('')
+	$('#result').show();
+	$.get(current_url,function(repos){
+		showCurrentRepositories(repos)
 	})
-}
+	 
 
-function getAjax(favName,favLang,favUrl){
-	var url="./js/favproject.json"
-
-	$.ajax({
-		url: url,
-		dataType: "json",
-		success: function(response){
-			$("#favresult").html('');
-			$("#favresult").show();
-			$("#favresult").fadeIn("slow",showFavResult(response));
-		}
-	})
-
-}
-
-function showFavResult(response) {
-	$.each(response,function(index,repo) {
-		var linkName= $('<a>').attr('href',repo.fav_url)
-		                      .text(repo.name)
-		                      .attr('id','fav_name')
-		                      .attr('target','_blank')
-	    var fav_language= $('<h5>').text(repo.language)
-	                               .attr('id','fav_lang')
-    $("#favresult").append(linkName,fav_language)
-
-	})
 }
