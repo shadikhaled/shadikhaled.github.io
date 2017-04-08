6fc3a4f6e5d8230d9c129cdfa1d310c8709029ec
Shadi Khaled <shadikhaled777@gmail.com>
2017-04-08 23:09:30 +0300

    add new function to skills.js

--- js/skills.js
+++ js/skills.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){
-	function getSkills(){
-skills_url = “skills.json”
-$.get(skills_url,function(data){
-	showSkills(data)
-
-
+$$(document).ready(function(){
+	getSkills();
 })
+function getSkills(){
+	skills_url = “skills.json”
+	$.get(skills_url,function(data){
+		showSkills(data)
+	});
+}
 	
 function getCurrentRepositories(e){
 	$('#result').html('')
@@ -14,5 +15,12 @@ function getCurrentRepositories(e){
 		showCurrentRepositories(repos)
 	})
 	 
+
+function showSkills(skills) {
+	$.each(sills,function(index,skill) {
+		var skill_item = "<div><h3><i class='"+skill.cssclass +"'></i>"+skill.name+"</h3>"+"<p>"+skill.description +"</p></div>";
+		$("#skills").append(skill_item)
+
+	})
 
 }
